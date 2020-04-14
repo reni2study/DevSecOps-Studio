6e497f764d8a00317d82c9ed30b7ab4c7db9b05e
secfigo <secfigo@gmail.com>
2020-04-14 23:35:00 +0800

    Bumped ruby version

--- provisioning/molecule/devsecops/tests/test_default.py
+++ provisioning/molecule/devsecops/tests/test_default.py
@@ -40,7 +40,7 @@ def test_directory_is_present(host, directory):
   '/usr/local/bin/ansible',
   '/usr/local/bin/ansible-lint',
   '/usr/local/bin/docker-compose',
-  '/usr/local/rvm/rubies/ruby-2.3.1/bin/ruby'
+  '/usr/local/rvm/rubies/ruby-2.6.3/bin/ruby'
 ])
 def test_binary_is_present(host, file):
     file = host.file(file)
@@ -79,8 +79,8 @@ def test_files(host, file, content):
 
 
 @pytest.mark.parametrize('command, regex', [
-  ("packer --version", "^1.0.0*"),
-  ("sudo su -c 'inspec --version' -l vagrant", "^3.2.6*")
+  ("packer --version", "^1.5.5*"),
+  ("sudo su -c 'inspec --version' -l vagrant", "^4.18.104*")
 ])
 def test_commands(host, command, regex):
     cmd = host.check_output(command)
